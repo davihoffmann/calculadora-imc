879a2bb4b981aaba6de6faca3a2830bc904e04c1
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-13 08:17:00 -0300

    criado formulario para pegar os dados

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,6 +14,38 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Icon(
+          Icons.person_outline,
+          size: 120,
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: "Peso (kg)",
+            labelStyle: TextStyle(color: Colors.black),
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: "Altura (cm)",
+            labelStyle: TextStyle(color: Colors.black),
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        )
+      ],
     );
   }
 }
