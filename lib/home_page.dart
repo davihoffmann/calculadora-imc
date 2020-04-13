d1a7346646c10709f2c7de660b9e2656bde093c6
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-13 08:28:42 -0300

    finbalizado formulario com estilização da página

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,32 +25,52 @@ class HomePage extends StatelessWidget {
   }
 
   _body() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Icon(
-          Icons.person_outline,
-          size: 120,
-        ),
-        TextField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Peso (kg)",
-            labelStyle: TextStyle(color: Colors.black),
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            size: 120,
           ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.black, fontSize: 25),
-        ),
-        TextField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Altura (cm)",
-            labelStyle: TextStyle(color: Colors.black),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (kg)",
+              labelStyle: TextStyle(color: Colors.black),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 25),
           ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.black, fontSize: 25),
-        )
-      ],
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.black),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 25),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            height: 50,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text(
+                "Calcular",
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            "Info",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.black, fontSize: 25),
+          ),
+        ],
+      ),
     );
   }
 }
